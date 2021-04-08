c9da173100c430dae42c532684ef2f990882c96e
Srivatsav98 <srivatsav.yechiraju98@gmail.com>
2021-04-08 20:54:41 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import Contact from './components/Contact';
 import Login from './components/Login';
+import AboutUs from './components/AboutUs';
 
 
 function App() {
@@ -13,10 +15,17 @@ function App() {
         <Route path="/login">
             <Login></Login>
           </Route>
-
+          <Route path="/contact" exact>
+            <Contact></Contact>
+          </Route>
+          <Route path="/about" exact>
+            <AboutUs></AboutUs>
+          </Route>
           <Route path="/" exact>
             <HomePage></HomePage>
           </Route>
+
+     
 
         
          
